edfaf7b63613d4a3cdd40b491103d15f52b951a8
IwraStudios <IwraStudios@hotmail.com>
2016-10-08 00:07:32 +0200

    Added stuff
    -Legacy parameters decoding
    - method of parameter-ing
    - Seed-based generation concept

---
+++ PassGen/PassGen.cs
@@ -0,0 +1,153 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+
+namespace Gravity.PassGen
+{
+    public class PassGenUtils
+    {
+        public string GenRandomPass(string Mask, int length1)
+        {
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length1--)
+            {
+                char c = Mask[rnd.Next(0, Mask.Length)];
+                //if ("+-&|!(){}[]^\"~*?:\\".Contains(c)) res.Append("\\\\");
+                res.Append(c);
+            }
+            return res.ToString();
+        }
+
+        /*public string GenTrueRandomPass(string Mask, int length1) //TODO: add plugin to PassGen
+        {
+            return null;
+        } */
+        public void LegacyParser(PassGenParameters parameters, out PassGenMaskOptions maskop)
+        {
+            maskop = new PassGenMaskOptions();
+            maskop.Init();
+            List<char[]> chars = new List<char[]>();
+            for (int i = 0; i > parameters.LegacyRules.Length; i = i + 2)
+            {
+                chars.Add(parameters.LegacyRules.Substring(i, 2).ToCharArray());
+            }
+            bool Record = false;
+            foreach (char[] ch in chars)
+            {
+                if (Record && ch[0] != '}')
+                {
+                    maskop.Mask.Add(ch[1].ToString().ToLower() + ch[1].ToString().ToUpper());
+                    continue;
+                }
+                switch (ch[0])
+                {
+                    case '?':
+                        switch (ch[1])
+                        {
+                            case 'l': //lowercase
+                                maskop.Mask.Add(maskop.DefaultMasks[0]); //abcdefghijklmnopqrstuvwxyz
+                                break;
+                            case 'u': //uppercase
+                                maskop.Mask.Add(maskop.DefaultMasks[1]); //ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                                break;
+                            case 'd': //decimal
+                                maskop.Mask.Add(maskop.DefaultMasks[2]); //1234567890
+                                break;
+                            case 'c'://any Case
+                                maskop.Mask.Add(maskop.DefaultMasks[0] + maskop.DefaultMasks[1]);
+                                break;
+                            case 't'://Three most common masks
+                                maskop.Mask.Add(maskop.DefaultMasks[0] + maskop.DefaultMasks[1] + maskop.DefaultMasks[2]);
+                                break;
+                            case 's': //special
+                                maskop.Mask.Add(maskop.DefaultMasks[3]); // !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+                                break;
+                            case 'a': //all
+                                maskop.Mask.Add(maskop.DefaultMasks[0] + maskop.DefaultMasks[1] + maskop.DefaultMasks[2] + maskop.DefaultMasks[3]); //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+                                break;
+
+                        }
+                        break;
+                    case '{':
+                        Record = true;
+                        break;
+                    case '}':
+                        Record = false;
+                        break;
+                }
+            }
+        }
+    }
+
+    public class PassGenGenerator
+    {
+        public string MaskString(string s, PassGenParameters parameters)
+        {
+            PassGenMaskOptions maskop;
+            if (parameters.UseLegacyRules)
+            {
+               new PassGenUtils().LegacyParser(parameters, out maskop);
+            }
+            else
+            {
+                maskop = new PassGenMaskOptions();
+                maskop.Init();
+                maskop.CustomMasks = new string[1];
+                maskop.CustomMasks[0] = parameters.changeMask;
+                for (int i = 0; i > parameters.additiveAmount; i++){ maskop.Mask.Add(parameters.additiveMask); }
+                foreach (char c in s.ToCharArray())
+                {
+                    maskop.Mask.Add(maskop.CustomMasks[0]);
+                }
+                for (int i = 0; i > parameters.additiveAmount; i++) { maskop.Mask.Add(parameters.additiveMask); }
+            }
+            ///Start Algorithm of Mask(input) Single time
+            
+            return null;
+        }
+        
+        public string MaskFromSeed(int seed, PassGenMaskOptions maskopt, out uint apos)
+        {
+            apos = 1;
+            foreach(string s in maskopt.Mask) apos = apos * (uint)s.Length;
+            return MaskFromSeed(seed, maskopt);
+        }
+        public string MaskFromSeed(int seed, PassGenMaskOptions maskopt)
+        {
+            return null;
+        }      
+    }
+
+
+    public class PassGenParameters
+    {
+        public bool UseLegacyRules;
+        public string LegacyRules;
+        public byte additiveAmount; //max added characters
+        public string additiveMask;
+        public byte changeAmount; //max Simultanius changed letters, additive not counted
+        public string changeMask;
+    }
+    public class PassGenMaskOptions
+    {
+        public List<string> Mask = new List<string>();
+        public string[] CustomMasks; //max 100
+        public string[] DefaultMasks; //Init first
+        public void Init()
+        {
+            DefaultMasks = new string[6];
+            DefaultMasks[0] = "abcdefghijklmnopqrstuvwxyz";
+            DefaultMasks[1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            DefaultMasks[2] = "1234567890";
+            DefaultMasks[3] = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        }
+    }
+
+    public class PassGenConst
+    {
+        public const string Mask_aZ09 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        public const string Mask_aZ09_SPcompatible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-+=";
+        public const string Mask_aZ09_SPall = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    }
+}
--- PassGen/Properties/AssemblyInfo.cs
+++ PassGen/Properties/AssemblyInfo.cs
@@ -1,8 +1,9 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PassGen")]
@@ -10,26 +11,19 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Project Gravity")]
 [assembly: AssemblyProduct("PassGen")]
-[assembly: AssemblyCopyright("2016 (GPLv3)")]
+[assembly: AssemblyCopyright("2016(GPLv3)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7deca43d-766c-48d9-a80e-83df58022f03")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
